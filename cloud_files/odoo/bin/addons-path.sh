2435e78d23c80f40db6ca6c55cb8e64391fe558e
jejemaes <jerome.2611@gmail.com>
2018-06-09 17:16:33 +0200

    [IMP] bin/openerp: new addons path

--- cloud_files/odoo/bin/addons-path.sh
+++ cloud_files/odoo/bin/addons-path.sh
@@ -1,23 +1,14 @@
 #!/usr/bin/env bash
 BRANCH=$1
-CUSTOM="$HOME/src/custom"
-THEMES="$HOME/src/themes"
-ENTERPRISE="$HOME/src/enterprise"
-ADDONS="$CUSTOM/default,$HOME/src/odoo/$BRANCH/addons"
-WORKER_ADDONS=web$(find "$CUSTOM/default" -maxdepth 1 -type d -name '*_worker' -printf ',%f')
+ADDONS="$HOME/src/odoo/$BRANCH/addons"
+WORKER_ADDONS=web
 
-MODES=$(jq -r .me.mode "$HOME/config/config.json" 2>/dev/null) || true
+declare -a REPOS=("jejemaes" "enterprise" "oca")
 
-if [[ -d "$THEMES/$BRANCH" ]]; then
-    ADDONS="$ADDONS,$THEMES/$BRANCH"
-fi
-if [[ -d "$ENTERPRISE/$BRANCH" ]]; then
-    ADDONS="$ENTERPRISE/$BRANCH,$ADDONS"    # enterprise repo must be in first place
-fi
-for m in $MODES; do
-    if [ -d "$CUSTOM/$m" ]; then
-        ADDONS="$CUSTOM/$m,$ADDONS"
+for m in $$REPOS; do
+    if [ -d "$HOME/src/$m" ]; then
+        ADDONS="$HOME/src/$m,$ADDONS"
         # each addons ending with '_worker' is considered as a worker to load
-        WORKER_ADDONS="$WORKER_ADDONS"$(find "$CUSTOM/$m" -maxdepth 1 -type d -name '*_worker' -printf ',%f')
+        WORKER_ADDONS="$WORKER_ADDONS"$(find "$HOME/src/$m" -maxdepth 1 -type d -name '*_worker' -printf ',%f')
     fi;
 done;
