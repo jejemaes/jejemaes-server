538ec8e02557a851206445da876fbb9691530830
jejemaes <jerome.2611@gmail.com>
2018-06-10 14:19:38 +0200

    [FIX] bin/openerp: fix addons-path

--- cloud_files/odoo/bin/addons-path.sh
+++ cloud_files/odoo/bin/addons-path.sh
@@ -3,12 +3,11 @@ BRANCH=$1
 ADDONS="$HOME/src/odoo/$BRANCH/addons"
 WORKER_ADDONS=web
 
-declare -a REPOS=("jejemaes" "enterprise" "oca")
-
-for m in $$REPOS; do
-    if [ -d "$HOME/src/$m/$BRANCH" ]; then
-        ADDONS="$HOME/src/$m/$BRANCH,$ADDONS"
+for entry in "$HOME/src"/*
+do
+  if [ -d "$entry/$BRANCH" ] && [ $entry != "$HOME/src/odoo" ]; then
+        ADDONS="$entry/$BRANCH,$ADDONS"
         # each addons ending with '_worker' is considered as a worker to load
-        WORKER_ADDONS="$WORKER_ADDONS"$(find "$HOME/src/$m" -maxdepth 1 -type d -name '*_worker' -printf ',%f')
+        WORKER_ADDONS="$WORKER_ADDONS"$(find "$entry/$BRANCH" -maxdepth 1 -type d -name '*_worker' -printf ',%f')
     fi;
 done;
