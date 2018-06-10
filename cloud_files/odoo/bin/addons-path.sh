169ba146972fa10b08d28e3dceda8ff59456856f
jejemaes <jerome.2611@gmail.com>
2018-06-10 14:01:29 +0200

    [IMP] bin/openerp: remove memory limit and fix addons-path and dbfilter

--- cloud_files/odoo/bin/addons-path.sh
+++ cloud_files/odoo/bin/addons-path.sh
@@ -6,8 +6,8 @@ WORKER_ADDONS=web
 declare -a REPOS=("jejemaes" "enterprise" "oca")
 
 for m in $$REPOS; do
-    if [ -d "$HOME/src/$m" ]; then
-        ADDONS="$HOME/src/$m,$ADDONS"
+    if [ -d "$HOME/src/$m/$BRANCH" ]; then
+        ADDONS="$HOME/src/$m/$BRANCH,$ADDONS"
         # each addons ending with '_worker' is considered as a worker to load
         WORKER_ADDONS="$WORKER_ADDONS"$(find "$HOME/src/$m" -maxdepth 1 -type d -name '*_worker' -printf ',%f')
     fi;
